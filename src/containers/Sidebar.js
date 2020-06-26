bbe34c60b14509c233b690f2382a8abace9ee730
navruz-zbx <iminov.zbx@gmail.com>
2020-06-26 17:53:14 +0500

    state haqida

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -3,12 +3,39 @@ import Link from '../components/Link'
 
 class Sidebar extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            number: 0,
+            data: [],
+            role: {
+                admin: true,
+                isNew: false
+            }
+        }
+    }
+
     clickHandler = () => {
-        alert('Clicking');
+        console.log('Clicked');
+        this.setState({number: this.state.number + 1}, () => this.alertHandler());
+    }
+
+    alertHandler = () => {
+        const {number} = this.state;
+        if(number === 5) {
+            alert('Number is 5');
+        }
+    }
+
+    clearHandler = () => {
+        this.setState({number: 0});
     }
 
     render() {
+        console.log(this.state);
         const {title, data} = this.props;
+        const {number} = this.state;
         return (
             <div>
                 <h1>Today.uz</h1>
@@ -16,6 +43,11 @@ class Sidebar extends Component {
                 <button onClick={this.clickHandler}>Click</button>
                 <hr />
                 <Link data={{url: '/my-link', title: 'Alitech', isActive: true}} />
+                <section>
+                    <h1>Number: {number}</h1>
+                    <button onClick={this.clickHandler}>Update number</button>
+                    <button onClick={this.clearHandler}>Clear</button>
+                </section>
             </div>
             
         )
