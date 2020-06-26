b6481117e0e7e553b1ff6aedef1c93b2516e11bf
navruz-zbx <iminov.zbx@gmail.com>
2020-06-27 01:41:09 +0500

    lifecycle unmounts

--- src/containers/Sidebar.js
+++ src/containers/Sidebar.js
@@ -1,55 +1,53 @@
 import React, { Component } from 'react';
-import Link from '../components/Link'
 
 class Sidebar extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            isLoading: true,
-            number: 0,
-            data: [],
-            role: {
-                admin: true,
-                isNew: false
+        this.state = {user:{name: '', img: ''}, num: 0}
+        console.log('Constructor........');
+    }
+
+    componentDidMount() {
+        console.log('ComponentDidMount.........');
+        // fetch user data from database: data ={name: '', img: ''}
+        this.setState({user:{name: 'Alisher', img: 'sfgdgdfgfga'}});
+    }
+
+    updateNumber = () => {
+        if(this.state.num < 4)
+        this.setState(oldState => {
+            return {
+                num: oldState.num + 1
             }
+        })  
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log(this.state.num, nextState.num);
+
+        return true;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log(prevState.num, this.state.num);
+        if (prevState.num < this.state.num){
+            console.log('hey');
         }
     }
 
-    clickHandler = () => {
-        console.log('Clicked');
-        this.setState({number: this.state.number + 1}, () => this.alertHandler());
-    }
-
-    alertHandler = () => {
-        const {number} = this.state;
-        if(number === 5) {
-            alert('Number is 5');
-        }
-    }
-
-    clearHandler = () => {
-        this.setState({number: 0});
+    componentWillUnmount() {
+        console.log('unmounting');
     }
 
     render() {
-        console.log(this.state);
-        const {title, data} = this.props;
-        const {number} = this.state;
+        console.log('Render.......');
+        const {num} = this.state;
         return (
             <div>
-                <h1>Today.uz</h1>
-                <p>{title}, {data.name}</p>
-                <button onClick={this.clickHandler}>Click</button>
-                <hr />
-                <Link data={{url: '/my-link', title: 'Alitech', isActive: true}} />
-                <section>
-                    <h1>Number: {number}</h1>
-                    <button onClick={this.clickHandler}>Update number</button>
-                    <button onClick={this.clearHandler}>Clear</button>
-                </section>
+                <h1>Hello React {num}</h1>
+                <button onClick={this.updateNumber}>Update</button>
             </div>
-            
         )
     }
 }
